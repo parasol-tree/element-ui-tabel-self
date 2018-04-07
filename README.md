00b1de5dc64fede25accba03be46e9ebab517c02
shilijingtian <shilijingtian@outlook.com>
2018-04-07 23:50:46 +0800

    Vue-elementUI_renderHeader

---
+++ README.md
@@ -0,0 +1,22 @@
+# element-ui-tabel-self
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn install
+吗卖批是  yarn install别弄错了
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column :render-header="renderHeader"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  methods: {
+    renderHeader (createElement, { iDonNotKnowWhatToPassOn }) {
+      return createElement(
+        'div',
+        {
+          'class': 'renderTableHead'
+        },
+        [
+          createElement(
+            'el-checkbox',
+            {
+              attrs: { class: 'shit' }
+            },
+            ['你这该死的温柔,让我心在痛泪在流,就在和你说分手以后,想忘记已不能够']
+          )
+        ]
+      )
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
